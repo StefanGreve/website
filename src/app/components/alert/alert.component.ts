5ff261411677d5dd8ea0a4a884e1b2043252f953
StefanGreve <greve.stefan@outlook.jp>
2024-01-22 22:48:19 +0100

    Add color and border to simple alert

--- src/app/components/alert/alert.component.ts
+++ src/app/components/alert/alert.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { Component, Input, OnInit, ViewEncapsulation } from "@angular/core";
 import { State } from "src/app/enums/state";
 import { Button } from "src/app/interfaces/button";
 import {v4 as uuid } from "uuid";
@@ -7,11 +8,14 @@ import {v4 as uuid } from "uuid";
   selector: "adv-alert",
   standalone: true,
   templateUrl: "./alert.component.html",
-  styleUrl: "./alert.component.scss"
+  styleUrl: "./alert.component.scss",
+  encapsulation: ViewEncapsulation.None,
+  imports: [CommonModule]
 })
 export class AlertComponent implements OnInit {
   public id!: string;
   public State = State;
+  public visible: boolean = true; // false
 
   @Input()
   public title!: string;
@@ -24,14 +28,20 @@ export class AlertComponent implements OnInit {
 
   public open() {
     console.log("opening alert");
+    this.visible = true;
+    document.body.classList.add("adv-alert-open");
   }
 
   public close() {
     console.log("closing alert");
+    this.visible = false;
+    document.body.classList.remove("adv-alert-open");
   }
 
   ngOnInit(): void {
     this.id = `alert__${uuid()}`;
     this.title ??= "Alert";
+    // line added for testing purposes temporarily
+    document.body.classList.add("adv-alert-open");
   }
 }
