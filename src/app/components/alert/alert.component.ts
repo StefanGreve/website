1c301de1e4ad2e0c820c6660ca05078bcf178f6d
StefanGreve <greve.stefan@outlook.jp>
2024-02-12 23:12:28 +0100

    Simplify overlay-related code

--- src/app/components/alert/alert.component.ts
+++ src/app/components/alert/alert.component.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from "@angular/common";
-import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit } from "@angular/core";
-import { HiddenDirective } from "src/app/directives/inputs/hidden.directive";
+import { Component, Input, OnInit } from "@angular/core";
 import { TitleDirective } from "src/app/directives/inputs/title.directive";
 import { State } from "src/app/enums/state";
 import { Button } from "src/app/interfaces/button";
@@ -11,39 +10,33 @@ import { v4 as uuid } from "uuid";
   standalone: true,
   templateUrl: "./alert.component.html",
   styleUrl: "./alert.component.scss",
-  changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [CommonModule],
   hostDirectives: [
     {
       directive: TitleDirective,
       inputs: ["title"],
     },
-    {
-      directive: HiddenDirective,
-      inputs: ["hidden"],
-    }
   ]
 })
-export class AlertComponent implements OnInit, AfterViewInit {
+export class AlertComponent implements OnInit {
   public readonly id: string = `adv__alert__${uuid()}`;
+  public hidden: boolean = true;
   public State = State;
 
   // directive inputs
   public title: string | undefined;
-  public hidden: boolean | undefined;
 
   @Input()
-  public content?: string;
+  public content: string | undefined;
 
   @Input()
-  public actions?: Button[];
+  public actions: Button[] | undefined;
 
   // eslint-disable-next-line no-unused-vars
-  constructor(private titleDirective: TitleDirective, private hiddenDirective: HiddenDirective, private ref: ChangeDetectorRef) { }
+  constructor(private titleDirective: TitleDirective) { }
 
   ngOnInit(): void {
     this.title = this.titleDirective.title;
-    this.hidden = this.hiddenDirective.hidden ?? true;
 
     // if there are only two buttons, then the primary button should be placed
     // on the right-hand side
@@ -52,24 +45,14 @@ export class AlertComponent implements OnInit, AfterViewInit {
     }
   }
 
-  ngAfterViewInit(): void {
-    this.ref.detectChanges();
-  }
-
   public open() {
-    console.debug(`[${this.id}] Opening alert`);
+    console.trace(`[${this.id}] Opening alert`);
     this.hidden = false;
-    this.ref.markForCheck();
+
   }
 
   public close() {
-    console.debug(`[${this.id}] Closing alert`);
+    console.trace(`[${this.id}] Closing alert`);
     this.hidden = true;
-
-    if (!this.hiddenDirective.hidden && !this.hidden) {
-      console.log("inside close if");
-      this.hidden = false;
-      this.ref.markForCheck();
-    }
   }
 }
--- src/app/components/dialog/dialog.component.ts
+++ src/app/components/dialog/dialog.component.ts
@@ -1,25 +1,24 @@
 import { CommonModule } from "@angular/common";
-import { Component, ViewEncapsulation } from "@angular/core";
-
+import { Component } from "@angular/core";
+import { v4 as uuid } from "uuid";
 
 @Component({
   selector: "adv-dialog",
   standalone: true,
   templateUrl: "./dialog.component.html",
   styleUrl: "./dialog.component.scss",
-  encapsulation: ViewEncapsulation.None,
   imports: [CommonModule]
 })
 export class DialogComponent {
-  public visible: boolean = false;
+  public readonly id: string = `adv__dialog__${uuid()}`;
+  public hidden: boolean = true;
 
   public open(): void {
-    this.visible = true;
-    document.body.classList.add("adv-dialog-open");
+    console.trace(`[${this.id}] Opening dialog`);
+    this.hidden = false;
   }
 
   public close(): void {
-    this.visible = false;
-    document.body.classList.remove("adv-dialog-open");
+    console.trace(`[${this.id}] Closing dialog`);
   }
 }
